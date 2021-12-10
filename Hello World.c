d95dc3cb29e2c0b2bee209ce62d8ee679cd33708
OliSew <oliviersewera@wp.pl>
2021-12-10 13:03:09 +0100

    Create Hello World.c

---
+++ Hello World.c
@@ -0,0 +1,9 @@
+// This is a program that checks that the c compiler is working well
+#include <stdio.h>
+
+int main() {
+    // C code is write here
+    printf("Hello world");
+    
+    return 0;
+}
